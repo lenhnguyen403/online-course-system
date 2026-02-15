f317e464f46ff56e295f4136365215a632af4a17
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2026-02-15 12:45:20 +0700

    refactor project

--- course-server/src/index.js
+++ course-server/src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import route from './routes/index.js';
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config()
 
 const port = process.env.PORT || 3003;
 
