a029b3628852be6e5df1881ac0f5fa90c0febe1a
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2026-03-04 12:47:57 +0700

    :bug: fix cors

--- course-server/src/server.js
+++ course-server/src/server.js
@@ -20,20 +20,7 @@ const port = process.env.PORT || 3003;
 cf.connectDB()
 cf.connectCloudinary()
 
-const allowedOrigins = [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'https://f5-edu-admin.vercel.app',
-    'https://f5-edu.vercel.app'
-
-]
-
-app.use(cors(
-    {
-        origin: allowedOrigins,
-        // credentials: true
-    }
-))
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
